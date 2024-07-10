a7c0d6b5725f5b01e45c105d8d2c11fb8f1f83c0
Liran <lirlib@hotmail.com>
2024-07-10 17:17:15 +0300

    Implement Upload to Firebase Storage

--- app/src/main/java/com/tallevi/petconnect/UploadPhoto.java
+++ app/src/main/java/com/tallevi/petconnect/UploadPhoto.java
@@ -57,7 +57,7 @@ public class UploadPhoto extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload_photo); // Ensure this is the correct layout
+        setContentView(R.layout.activity_upload_photo);
 
         FirebaseApp.initializeApp(UploadPhoto.this);
         storageReference = FirebaseStorage.getInstance().getReference();
@@ -69,6 +69,18 @@ public class UploadPhoto extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         selectImage = findViewById(R.id.selectImage);
         uploadImage = findViewById(R.id.uploadImage);
+        MaterialButton backToMainScreen = findViewById(R.id.backToMainScreen);
+
+        // Set click listener for back to main screen button
+        backToMainScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate back to MainActivity
+                Intent intent = new Intent(UploadPhoto.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Close the current activity
+            }
+        });
 
         selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
